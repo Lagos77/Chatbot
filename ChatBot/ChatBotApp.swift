ec68195ed395c39100ec389522d1173f5b1086c5
Lagos77 <social_a@live.com>
2021-12-31 14:43:10 -0500

    Initial Commit

---
+++ ChatBot/ChatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBotApp.swift
+//  ChatBot
+//
+//  Created by A J on 2021-12-31.
+//
+
+import SwiftUI
+
+@main
+struct ChatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
